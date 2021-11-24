a85a8dff01d720c4fcd44d6c5bca4d63150365d3
Batya02 <magomedov.abdul20012@gmail.com>
2021-11-24 22:25:37 +0300

    detected api bug and fixed

--- AeroWeb/core/urls.py
+++ AeroWeb/core/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("", index, name="index"),
-    path("api/",  UsersView.as_view(), name="users-api"),
+    #path("api/",  UsersView.as_view(), name="users-api"),
     path("api/user-login/<str:last_name>/<str:department>/<str:password>/", UserLogin.as_view(), name="user-login"),
     path("api/user-update-active/<str:last_name>/<int:active>", UserUpdateActive.as_view(), name="user-update-active"),
     path("api/user-register/<str:last_name>/<str:first_name>/<str:middle_name>/<str:position>/<str:department>/<str:password>/",
