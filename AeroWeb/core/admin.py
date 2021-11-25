f7cc416f6cb62c6ffb1488f23a043ed3428156a9
Batya02 <magomedov.abdul20012@gmail.com>
2021-11-25 14:04:10 +0300

    add admin settings

--- AeroWeb/core/admin.py
+++ AeroWeb/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Users)
+class Users(admin.ModelAdmin):
+    fields = ["last_name", "first_name", "middle_name", "position",
+              "department", "password", "created", "is_active"]
+    list_display = ["pk", "last_name", "created"]
+    readonly_fields = ["is_active"]
+    search_fields = ["last_name"]
+
+
+@admin.register(NotifyError)
+class NotifyErrorAdmin(admin.ModelAdmin):
+    fields = ["last_name", "text", "created"]
+    list_display = ["pk", "last_name", "created"]
+    search_fields = ["last_name"]
